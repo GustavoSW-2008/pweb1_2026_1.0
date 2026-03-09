45c8a1085467bbbb93b2983cf5a01942a11691a6
GustavoSW-2008 <gustavo.sw2008@aluno.ifsc.edu.br>
2026-03-08 21:32:02 -0300

    Add Lista de Exercicios2

---
+++ Exercício Prático 02/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício Prático 02</title>
+</head>
+<body>
+    <h1>Exercícios Práticos</h1>
+    <ul>
+        <li><a href="matricula.html">Exercício 1: Matrícula de Alunos</a></li>
+        <li><a href="exercicio2.html">Exercício 2: Ficha Cadastral</a></li>
+        <li><a href="exercicio3.html">Exercício 3: Lista de Inscritos</a></li>
+    </ul>
+</body>
+</html>
